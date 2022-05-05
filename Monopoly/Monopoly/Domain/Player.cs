c4bdce2c25be9c6247359f307ec2ef9251d9fd81
Katrine Birk <katrinebirk2@live.dk>
2022-05-05 10:05:46 +0200

    Added methods and attributes to player class

--- Monopoly/Monopoly/Domain/Player.cs
+++ Monopoly/Monopoly/Domain/Player.cs
@@ -8,6 +8,24 @@ namespace Monopoly
 {
     internal class Player
     {
+        private String name;
+        private int balance = 1500;
+        private List<Property> properties;
+        private bool jailed = false;
+
+        public Player (String name) {
+            this.name = name; 
+        }
+
+        public void setBalance(int balance)
+        {
+            this.balance = balance;
+        }
+
+        public int getBalance()
+        {
+            return balance;
+        }
 
     }
 }
